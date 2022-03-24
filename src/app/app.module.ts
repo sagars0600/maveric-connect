c7e6fcf964f3b23117c6111a8089685eeee8b6e2
sagars0600 <sagarsolanki2120@gmail.com>
2022-03-24 16:22:23 +0530

    sign-up added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { HeaderComponent } from './header/header.component';
+import { SignupComponent } from './signup/signup.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
